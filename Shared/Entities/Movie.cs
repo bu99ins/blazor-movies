d84bab559905268842d80992711439c8dbe13017
Yury Krasavin <buggins@live.com>
2020-02-14 22:05:04 +0300

    create the Movie entity

---
+++ Shared/Entities/Movie.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace BlazorMovies.Shared.Entities
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
