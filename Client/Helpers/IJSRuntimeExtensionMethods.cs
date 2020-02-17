d3d971d7f50cbf338a4f195f2a606c05333746b9
Yury Krasavin <buggins@live.com>
2020-02-17 23:37:19 +0300

    extract js invocation to an extension method

---
+++ Client/Helpers/IJSRuntimeExtensionMethods.cs
@@ -0,0 +1,13 @@
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+
+namespace BlazorMovies.Client.Helpers
+{
+    public static class IJSRuntimeExtensionMethods
+    {
+        public static async ValueTask<bool> Confirm(this IJSRuntime js, string message)
+        {
+            return await js.InvokeAsync<bool>("confirm", message);
+        }
+    }
+}
